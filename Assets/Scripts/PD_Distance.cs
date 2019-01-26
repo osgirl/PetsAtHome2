a7cdc93649022d4c32f05a7611657faf6ab7bad2
Miles-Tone <mileswar@me.com>
2019-01-26 19:10:55 +0000

    audio engine in unity first steps

--- Assets/Scripts/PD_Distance.cs
+++ Assets/Scripts/PD_Distance.cs
@@ -10,6 +10,13 @@ public class PD_Distance : MonoBehaviour
     [Range(0, 1)]
     public float hue = 1;
 
+    public bool motifToggle0;
+    public bool motifToggle1;
+    public bool motifToggle2;
+    public bool motifToggle3;
+    public bool motifToggle4;
+    public bool motifToggle5;
+
     private void Start()
     {
         if (pdInstance == null)
@@ -20,5 +27,12 @@ public class PD_Distance : MonoBehaviour
     {
         pdInstance.SendFloat("distance", distance);
         pdInstance.SendFloat("hueAngle", hue);
+        pdInstance.SendFloat("motifLayer0", motifToggle0 ? 1.0f : 0.0f);
+        pdInstance.SendFloat("motifLayer1", motifToggle1 ? 1.0f : 0.0f);
+        pdInstance.SendFloat("motifLayer2", motifToggle2 ? 1.0f : 0.0f);
+        pdInstance.SendFloat("motifLayer3", motifToggle3 ? 1.0f : 0.0f);
+        pdInstance.SendFloat("motifLayer4", motifToggle4 ? 1.0f : 0.0f);
+        pdInstance.SendFloat("motifLayer5", motifToggle5 ? 1.0f : 0.0f);
+
     }
 }
