afdd81a4a6fe3ba48315b920fe3de551fa3fc152
Jake Howden <jakehowden@newparksolutions.com>
2019-01-26 16:28:16 +0000

    fewFEW

--- Assets/Win.cs
+++ Assets/Win.cs
@@ -1,20 +1,22 @@
 ﻿using System.Collections;
+using System.Linq;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Win : MonoBehaviour
 {
 
+    List<GameObject> Pillars;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        Pillars = GameObject.FindGameObjectsWithTag("Pillar").ToList();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        int count = Pillars.Where(x => x.)   
     }
 }
