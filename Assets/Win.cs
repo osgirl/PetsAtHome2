13d43d2b3f23abdd962509ecd97db9ab7ef2c6e3
Jake Howden <jakehowden@newparksolutions.com>
2019-01-26 17:29:05 +0000

    win condition

---
+++ Assets/Win.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class Win : MonoBehaviour
+{
+    List<GameObject> Pillars = new List<GameObject>();
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Pillars = GameObject.FindGameObjectsWithTag("Pillar").ToList();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Pillars.Count() > 30)
+        {
+
+        }
+    }
+}
