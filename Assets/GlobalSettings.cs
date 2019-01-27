0b892363dfbbdae5843261bdc6a6a54018c6e1c6
Nichotine <henrylogannicholson@gmail.com>
2019-01-27 14:39:56 +0000

    updated globals

--- Assets/GlobalSettings.cs
+++ Assets/GlobalSettings.cs
@@ -4,9 +4,15 @@ using UnityEngine;
 
 public class GlobalSettings : MonoBehaviour
 {
-	public static float worldColourScale = 512f;
+	public static float worldColourScale, worldSize;
+	[SerializeField]
+	private float whiteRatio;
 	public Material worldHSV;
+	public Transform land;
 	void Start(){
+		
+		worldColourScale = land.localScale.x * whiteRatio / 2f;
+		worldSize = land.localScale.x;
 		worldHSV.SetFloat("_ScaleFloat", worldColourScale);
 	}
 }
