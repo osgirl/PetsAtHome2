d2d77de98bf0ecf59a04d87b2c68170fcea7dde2
Nichotine <henrylogannicholson@gmail.com>
2019-01-27 13:51:01 +0000

    Update HSV_ScaledSaturation.cs

--- Assets/Scripts/HSV_ScaledSaturation.cs
+++ Assets/Scripts/HSV_ScaledSaturation.cs
@@ -12,6 +12,6 @@ public class HSV_ScaledSaturation : HSV_Base
     }
     void Update()
     {
-        rend.material.SetColor("_Color",Color.HSVToRGB((Mathf.Atan2(transform.position.x * invertFloat, transform.position.z * invertFloat) + Mathf.PI)/(2*Mathf.PI),Vector3.Distance(Vector3.zero,transform.position)/scaleValue,1));
+        rend.material.SetColor("_Color",Color.HSVToRGB((Mathf.Atan2(transform.position.x * invertFloat, transform.position.z * invertFloat) + Mathf.PI)/(2*Mathf.PI),Vector3.Distance(Vector3.zero,transform.position)/GlobalSettings.worldColourScale,1));
     }
 }
