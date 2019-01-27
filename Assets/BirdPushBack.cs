8df011b1edaa14adad2f4911b27954710933bdad
lonewolfdesign <thomaswoodthorpe@gmail.com>
2019-01-27 14:31:09 +0000

    FixedPillars

---
+++ Assets/BirdPushBack.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BirdPushBack : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    void OnCollisionEnter(Collision other){
+       
+        
+             Debug.Log("ENTERTRIGGER");
+            this.gameObject.transform.Translate(Vector3.back * 10);
+        
+    }
+}
