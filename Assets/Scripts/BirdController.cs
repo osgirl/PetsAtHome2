cfca67e6fc6063dd59b31f4f753b77d8f4cf4ac6
Nichotine <henrylogannicholson@gmail.com>
2019-01-29 15:30:35 +0000

    tidied bird controller

--- Assets/Scripts/BirdController.cs
+++ Assets/Scripts/BirdController.cs
@@ -9,30 +9,24 @@ public class BirdController : MonoBehaviour
     [SerializeField]
     private Vector3 leftJoystick, rightJoystick, keyboardMovement;
     [SerializeField]
-    private float speed, defaultSpeed, forwardFloat, rotateSpeed, rotationValue = 20f, turnSpeed;
-    private Action execution;
+    private float speed, defaultSpeed, forwardFloat, rotateSpeed, rotationValue = 20f, turnSpeed = 4f;
     [SerializeField]
-    private Transform cameraSocket, birdSocket;
-    private Camera camera;
-    [SerializeField]
-    private bool testMode;
+    private Transform birdSocket;
 	public static float speedReference;
     void Start(){
-        camera = Camera.main;
         characterController = GetComponent<CharacterController>();
     }
     void Update()
     {
-		leftJoystick = new Vector3(Input.GetAxis("Left Joystick X"),0,Input.GetAxis("Left Joystick Y"));
-		rightJoystick = new Vector3(Input.GetAxis("Right Joystick X"),0, Input.GetAxis("Right Joystick Y"));
+        forwardFloat = defaultSpeed;
 		keyboardMovement = new Vector3(Input.GetAxis("Horizontal NEW"),0,Input.GetAxis("Vertical NEW"));
-
-		forwardFloat = defaultSpeed;
 		if (keyboardMovement.magnitude > 0) {
 			transform.Rotate(0, keyboardMovement.x * turnSpeed, 0);
 			forwardFloat += keyboardMovement.z * speed * (1 - Mathf.Abs(keyboardMovement.x)/2);
 			birdSocket.localRotation = Quaternion.RotateTowards(birdSocket.localRotation,Quaternion.Euler((keyboardMovement.z*rotationValue)*0.2f,0, keyboardMovement.x *-rotationValue),Time.deltaTime * rotateSpeed);
 		} else {
+            leftJoystick = new Vector3(Input.GetAxis("Left Joystick X"),0,Input.GetAxis("Left Joystick Y"));
+		    rightJoystick = new Vector3(Input.GetAxis("Right Joystick X"),0, Input.GetAxis("Right Joystick Y"));
 			transform.Rotate(0, (leftJoystick.z - rightJoystick.z) * turnSpeed / 2f, 0);
 			forwardFloat += (leftJoystick.z + rightJoystick.z) * speed / 2f;
 			birdSocket.localRotation = Quaternion.RotateTowards(birdSocket.localRotation,Quaternion.Euler((leftJoystick.z + rightJoystick.z)*rotationValue / 2f,0, (leftJoystick.z - rightJoystick.z) * -rotationValue /2f),Time.deltaTime * rotateSpeed);
